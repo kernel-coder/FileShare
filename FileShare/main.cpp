c55ac3253f11ff56f3d48503b5df45e2151b45f5
Shahazan Ali <kernel.writer@gmail.com>
2017-04-12 01:57:32 +0600

    Socket made to antoher thread

--- FileShare/main.cpp
+++ FileShare/main.cpp
@@ -58,7 +58,6 @@ int main(int argc, char *argv[])
 {
     atexit(appCleanup);
     qRegisterMetaType<QMap<QString, QByteArray>>("QMap<QString,QByteArray>");
-    qRegisterMetaType<QHostAddress>("QHostAddress");
     qInstallMessageHandler(customMessageHandler);
 
     QGuiApplication app(argc, argv);
